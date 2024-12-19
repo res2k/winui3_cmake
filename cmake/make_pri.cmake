c7cecd49395ba011acb9dde69608d80db89dcce3
Frank Richter <frank.richter@gmail.com>
2024-12-19 16:30:08 +0100

    Add make_pri_postbuild() function

--- cmake/make_pri.cmake
+++ cmake/make_pri.cmake
@@ -10,16 +10,10 @@ function(_file_generate_from_list FILE LIST)
     file(GENERATE OUTPUT "${FILE}" CONTENT "${joined}")
 endfunction()
 
-# Generate a .pri file
-#
-# OUTPUT - Name of .pri to generate
-# DIRECTORY - Directory containing files to include in .pri
-# FILES - Files include in .pri
-function(make_pri)
-    cmake_parse_arguments(makepri "" "OUTPUT;DIRECTORY" "FILES" ${ARGV})
-
+# Helper: Generate input files for MAKEPRI
+function(_make_pri_prepare PRICONFIG_VAR makepri_OUTPUT makepri_DIRECTORY)
     set(resfiles_path "${makepri_OUTPUT}.resfiles")
-    _file_generate_from_list("${resfiles_path}" "${makepri_FILES}")
+    _file_generate_from_list("${resfiles_path}" "${ARGN}")
 
     set(priconfig_path "${makepri_OUTPUT}.xml")
     set(priconfig_contents "")
@@ -46,13 +40,51 @@ function(make_pri)
     list(APPEND priconfig_contents "</resources>")
     _file_generate_from_list("${priconfig_path}" "${priconfig_contents}")
 
+    set(${PRICONFIG_VAR} "${priconfig_path}" PARENT_SCOPE)
+endfunction()
+
+# Helper: Assemble MAKEPRI command
+function(_make_pri_command COMMAND_VAR makepri_OUTPUT priconfig_path)
+    set(${COMMAND_VAR} powershell "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/wrap_makepri.ps1" "'${MAKEPRI}' new /cf '${priconfig_path}' /pr '${CMAKE_CURRENT_SOURCE_DIR}' /o /of '${makepri_OUTPUT}'" PARENT_SCOPE)
+endfunction()
+
+# Generate a .pri file
+#
+# OUTPUT - Name of .pri to generate
+# DIRECTORY - Directory containing files to include in .pri
+# FILES - Files include in .pri
+function(make_pri)
+    cmake_parse_arguments(makepri "" "OUTPUT;DIRECTORY" "FILES" ${ARGV})
+    require_arguments(makepri OUTPUT DIRECTORY)
+
+    _make_pri_prepare(priconfig_path "${makepri_OUTPUT}" "${makepri_DIRECTORY}" ${makepri_FILES})
+
     set(depend_paths "")
     foreach(file ${makepri_FILES})
         list(APPEND depend_paths "${makepri_DIRECTORY}/${file}")
     endforeach()
 
+    _make_pri_command(pri_command "${makepri_OUTPUT}" "${priconfig_path}")
     add_custom_command(OUTPUT "${makepri_OUTPUT}"
-                       COMMAND powershell "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/wrap_makepri.ps1" "'${MAKEPRI}' new /cf '${priconfig_path}' /pr '${CMAKE_CURRENT_SOURCE_DIR}' /o /of '${makepri_OUTPUT}'"
+                       COMMAND ${pri_command}
                        DEPENDS ${depend_paths}
                       )
+endfunction()
+
+# Generate a .pri file as post-build step to TARGET
+#
+# TARGET - Target to generate .pri file for
+# OUTPUT - Name of .pri to generate
+# DIRECTORY - Directory containing files to include in .pri
+# FILES - Files include in .pri
+function(make_pri_postbuild TARGET)
+    cmake_parse_arguments(makepri "" "OUTPUT;DIRECTORY" "FILES" ${ARGN})
+    require_arguments(makepri OUTPUT DIRECTORY)
+
+    _make_pri_prepare(priconfig_path "${makepri_OUTPUT}" "${makepri_DIRECTORY}" ${makepri_FILES})
+
+    _make_pri_command(pri_command "${makepri_OUTPUT}" "${priconfig_path}")
+    add_custom_command(TARGET ${TARGET}
+                       POST_BUILD
+                       COMMAND ${pri_command})
 endfunction()
